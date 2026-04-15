63d7db39dc4975dedca4100d22e90538d1025e31
MaxGraey <maxgraey@gmail.com>
2026-04-15 20:06:32 +0300

    exclude src/.vuepress/.cache

--- src/.vuepress/.cache/deps/@vue_devtools-api.js
+++
@@ -1,2 +0,0 @@
-import { a as removeCustomCommand, i as onDevToolsConnected, n as addCustomTab, o as setupDevToolsPlugin, r as onDevToolsClientConnected, t as addCustomCommand } from "./dist-jlnTmUNc.js";
-export { addCustomCommand, addCustomTab, onDevToolsClientConnected, onDevToolsConnected, removeCustomCommand, setupDevToolsPlugin, setupDevToolsPlugin as setupDevtoolsPlugin };
